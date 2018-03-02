5b4cdf307e38bb57cd967a6019c798db0181b167
Jarl-Otto Siikasmaa <jarl-ottosiikasmaa@macbookpro.lan>
2018-03-02 13:50:56 +0200

    First commit

---
+++ reader.py
@@ -0,0 +1,72 @@
+from wand.image import Image
+from PIL import Image as PI
+from PIL import ImageDraw
+import pyocr
+import pyocr.builders
+import io
+import re
+import os
+
+def setup(pref_lang, path):
+    tool = pyocr.get_available_tools()[0]
+    lang = pref_lang if pref_lang in tool.get_available_languages() else "eng"
+
+    req_image = []
+    result_dict = []
+
+    image_pdf = Image(filename=path, resolution=300)
+    image_pdf.save(filename=os.path.abspath("./temp.png"))
+    image_jpeg = image_pdf.convert('jpeg')
+
+    for img in image_jpeg.sequence:
+        img_page = Image(image=img)
+        req_image.append(img_page.make_blob('jpeg'))
+
+    page = 0
+    for img in req_image:
+        line = tool.image_to_string(
+            PI.open(io.BytesIO(img)),
+            lang=lang,
+            builder=pyocr.builders.LineBoxBuilder()
+        )
+        for line_item in line:
+            tmp = {"text": line_item.content, "pos": line_item.position, "page": page}
+            result_dict.append(tmp)
+        page += 1
+
+    return result_dict
+
+def visualize_result(result_dict):
+    for z in range(0,result_dict[len(result_dict)-1]['page']):
+        im = PI.open(os.path.abspath("./temp-" + str(z) + ".png"))
+        draw = ImageDraw.Draw(im)
+        for x in result_dict:
+            if x['page'] == z:
+                draw.rectangle((x['pos']), outline=(255, 0, 0, 255))
+        del draw
+        im.show()
+
+def search(result_dict, search_value):
+    offset = 85
+    for x in result_dict:
+        if search_value.lower().decode('utf-8') in x['text'].lower():
+            search_x = (x['pos'][0][0]+x['pos'][1][0])/2
+            search_y = (x['pos'][0][1]+x['pos'][1][1])/2
+            for x in result_dict:
+                target_x = (x['pos'][0][0]+x['pos'][1][0])/2
+                target_y = (x['pos'][0][1]+x['pos'][1][1])/2
+                if target_x >= search_x-offset and target_x <= search_x+offset and target_y <= search_y+offset and target_y >= search_y-offset and re.match('[0-9]+.*', x['text']):
+                    return x['text']
+
+def table_search(result_dict):
+    column_limits = (0,304,522,1130,1480,1700,1980,3000)
+    column_result = {}
+    row_limits_page0 = (1180,3190)
+    row_limits_else = (590, 3170)
+    for column in range(len(column_limits)-1):
+        column_result['column'+str(column)] = []
+        for x in result_dict:
+            row_limits = row_limits_page0 if x['page'] == 0 else row_limits_else
+            if x['pos'][0][0] > column_limits[column] and x['pos'][0][0] < column_limits[column+1] and x['pos'][0][1] > row_limits[0] and x['pos'][0][1] < row_limits[1]:
+                column_result['column'+str(column)].append(x['text'])
+    return column_result
